309fa5d6a280c97636b0019e255b239ee3f50428
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-03 19:25:22 +0200

    MILESTONE 1

---
+++ JS/funzioni.js
@@ -0,0 +1,19 @@
+
+// //creo variabile bomb
+// let num = "";
+
+// //creo un array di bombe
+// let arrayNum = []
+
+// for (let i = 0; arrayNum.length < 5; i++) {
+//     num = Math.floor(Math.random() * 100) + 1;
+//     //se il numero è già presente --> nun facciamo nulla
+//     if (arrayNum.includes(num)) {
+//     }
+//     //altrimenti viene pushato nell'array
+//     else {
+//         console.log("Le bombe sono la numero:", num)
+//         arrayNum.push(num)
+//         console.log(arrayNum)
+//     }
+// }
