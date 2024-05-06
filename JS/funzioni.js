5b6b6bca60b9c1e83dc25786c61e8606b0847341
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-06 15:24:32 +0200

    MILESTONE 3 Dentro la funzione temporizzata: - inserire 5 prompt nella funzione temporizzata

--- JS/funzioni.js
+++ JS/funzioni.js
@@ -1,19 +1,72 @@
+// CREO FUNZIONE PER NUMERI CASUALI CHE NON SI RIPETONO
+function casuale() {
+    let num = "";
+    let arrayNum = []
+    for (let i = 0; arrayNum.length < 5; i++) {
+        num = Math.floor(Math.random() * 100) + 1;
+        //se il numero è già presente --> nun facciamo nulla
+        if (arrayNum.includes(num)) {
+        }
+        //altrimenti viene pushato nell'array
+        else {
+            console.log("I numeri generati sono:", num)
+            arrayNum.push(num)
+            console.log(arrayNum)
 
-// //creo variabile bomb
-// let num = "";
+        }
+        document.getElementById("casuale").innerHTML = "Numeri casuali " + arrayNum;
+    }
+    setInterval(function () {
+        //  parte il conto alla rovescia
+        if (seconds != 0) {
+            console.log(seconds)
+            document.getElementById("seconds").innerText = seconds
+            seconds--
+        } else {
+            // allo 0 i numeri sparisconoS
+            document.getElementById("casuale").innerText = "Prova ad indovinare i numeri casuali"
+            //clearInterval(hideNum)
+        }
 
-// //creo un array di bombe
-// let arrayNum = []
 
-// for (let i = 0; arrayNum.length < 5; i++) {
-//     num = Math.floor(Math.random() * 100) + 1;
-//     //se il numero è già presente --> nun facciamo nulla
-//     if (arrayNum.includes(num)) {
-//     }
-//     //altrimenti viene pushato nell'array
-//     else {
-//         console.log("Le bombe sono la numero:", num)
-//         arrayNum.push(num)
-//         console.log(arrayNum)
-//     }
-// }
+
+
+    }, treSec);
+
+    return arrayNum
+}
+
+
+// CREO FUNZIONE PER PER CHIEDERE I NUM CASUALI TRAMITE PROMPT ALL'UTENTE
+
+// FACCIO PARTIRE I PROMPT DOPO 10 SEC
+let numUtente = setTimeout(function () {
+    let numPrompt = 0;
+    let arrayPrompt = []
+    for (let i = 0; arrayPrompt.length < 5; i++) {
+        numPrompt = prompt("quali erano i numeri?")
+        //se il numero è già presente --> nun facciamo nulla
+        if (arrayPrompt.includes(numPrompt)) {
+            console.log("C'hai preso")
+        }
+        //altrimenti viene pushato nell'array
+        else {
+            arrayPrompt.push(numPrompt)
+            console.log(arrayPrompt)
+
+        }
+        console.log("Me pare sianooo:", numPrompt)
+        document.getElementById("casualeUtente").innerHTML = "Numeri utente " + arrayPrompt;
+
+
+        console.log("i numeri scelti dall'utente sono:", arrayPrompt)
+    }
+    return arrayPrompt
+}, 10000)
+
+
+
+
+
+
+
--- JS/main.js
+++ JS/main.js
@@ -1,64 +1,14 @@
 
-// // - Devo andare a generare 5 numeri casuali. 
-// //creo variabile bomb 
-// let num = "";
 
-// //creo un array di bombe
-// let arrayNum = []
 
-// for (let i = 0; arrayNum.length < 5; i++) {
-//     num = Math.floor(Math.random() * 100) + 1;
-//     //se il numero è già presente --> nun facciamo nulla
-//     if (arrayNum.includes(num)) {
-//     }
-//     //altrimenti viene pushato nell'array
-//     else {
-//         console.log("Le bombe sono la numero:", num)
-//         arrayNum.push(num)
-//         console.log(arrayNum)
-//     }
-// }
-
-// - Generate 5 numeri casuali.
-
-function casuale() {
-    let num = "";
-    let arrayNum = []
-    for (let i = 0; arrayNum.length < 5; i++) {
-        num = Math.floor(Math.random() * 100) + 1;
-        //se il numero è già presente --> nun facciamo nulla
-        if (arrayNum.includes(num)) {
-        }
-        //altrimenti viene pushato nell'array
-        else {
-            console.log("Le bombe sono la numero:", num)
-            arrayNum.push(num)
-            console.log(arrayNum)
-
-        }
-        document.getElementById("casuale").innerHTML = "Numeri casuali " + arrayNum;
-    }
-    return arrayNum
-}
-
-// - salvare i dati in una variabile.
-let numCasuali = casuale()
-
-// tempo per ogni ripetizione
-let treSec = 3 * 1000
-
+// // tempo per ogni ripetizione
+let treSec = 2 * 1000
 let seconds = 3
 
-// setInterval(nomeFunzione, tempoPerOgniRipetizione);
-let hideNum = setInterval(function () {
-    if (seconds != 0) {
-        console.log(seconds)
-        document.getElementById("seconds").innerText = seconds
-        seconds--
-    } else {
-        document.getElementById("casuale").innerText = ""
-        clearInterval(hideNum)
-    }
 
-}, treSec);
-console.log(hideNum)
+let button = document.getElementById("button");
+
+// button.addEventListener("click", function ())
+
+
+
